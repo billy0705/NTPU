eafdf61e9ed7094bcd57d62af316a509e5903ba7
billy0705 <43518865+billy0705@users.noreply.github.com>
2019-11-22 16:32:02 +0800

    Add files via upload

---
+++ to_dataset_new.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+data_dir = './training'
+class_num = 10
+
+ori_data_dir = {}
+data_list = {}
+
+for i in range(class_num):
+    ori_data_dir[i] = os.path.join(data_dir, str(i))
+    data_list[i] = os.listdir(ori_data_dir[i])
+    print(len(data_list[i]))
+
+train_class_dir = {}
+validation_class_dir = {}
+base_dir = './dataset'
+if not os.path.isdir(base_dir):
+  os.mkdir(base_dir)
+train_dir = os.path.join(base_dir, "train")
+if not os.path.isdir(train_dir):
+  os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, "validation")
+if not os.path.isdir(validation_dir):
+  os.mkdir(validation_dir)
+
+for i in range(class_num):
+    if i % 10 < 10:
+        train_class_dir[i] = os.path.join(train_dir, str(i))
+        if not os.path.isdir(train_class_dir[i]):
+            os.mkdir(train_class_dir[i])
+        validation_class_dir[i] = os.path.join(validation_dir, str(i))
+        if not os.path.isdir(validation_class_dir[i]):
+            os.mkdir(validation_class_dir[i])
+        fnames = data_list[i][:400]
+        for fname in fnames:
+            src = os.path.join(ori_data_dir[i], fname)
+            dst = os.path.join(validation_class_dir[i], fname)
+            shutil.copyfile(src, dst)
+        fnames = data_list[i][400:]
+        for fname in fnames:
+            src = os.path.join(ori_data_dir[i], fname)
+            dst = os.path.join(train_class_dir[i], fname)
+            shutil.copyfile(src, dst)
+        print("Finish ", str(i))
